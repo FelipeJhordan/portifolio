03307b44a62a427939f9e96acbafd262630cd0bf
Felipe Jhordan <felipejordan.alves@gmail.com>
2021-08-18 10:39:13 -0300

    Add Routes

---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Route, BrowserRouter } from 'react-router-dom'
+
+import Home from '../Containers/Home'
+import Main from '../Containers/Main'
+import Welcome from '../Containers/Welcome/index.jsx/index.jsx'
+
+const Routes =  () => {
+    return (
+        <BrowserRouter>
+            <Route component={Home} path="/" exact />
+            <Route component={Welcome} path="/welcome" exact />
+            <Route component={Main} path="/main" exact />
+        </BrowserRouter>
+    )
+}
+
+export default Routes
