2429e192f0a3f403cbf939e05e88913064cd3781
Felipe Jhordan <felipejordan.alves@gmail.com>
2021-08-18 10:40:42 -0300

    Add file for link of all project images

---
+++ src/assets/img/projects.js
@@ -0,0 +1,14 @@
+import Project0 from './project0.png'
+import Project1 from './project1.png'
+import Project2 from './project2.png'
+import Project3 from './project3.png'
+
+
+const Projects = {
+    Project0,
+    Project1,
+    Project2,
+    Project3
+}
+
+export default Projects
